9f3234a0aaafada418c729d72e0e65a4a7aa5489
EleckiyVladislav <eleckiy_common@mail.ru>
2022-12-13 23:52:13 +0300

    Done RequestListRepImplementation.kt

--- app/src/main/java/ru/myrequest/data/RequestListRepImplementation.kt
+++ app/src/main/java/ru/myrequest/data/RequestListRepImplementation.kt
@@ -1,16 +1,19 @@
 package ru.myrequest.data
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import ru.myrequest.domain.RequestItem
 
 object RequestListRepImplementation {
 
-    private val requestList = Mutabl
+    private var requestListLD = MutableLiveData<List<RequestItem>>()
+    private val requestList = mutableListOf<RequestItem>()
 
     private var autoIncrementId = 0
 
     init {
         for (i in 0 until 10) {
-            val item = RequestItem(
+            val requestItem = RequestItem(
                 "Москва",
                 "5",
                 "ул.Мира",
@@ -20,14 +23,42 @@ object RequestListRepImplementation {
                 "Диагностика",
                 "01.01.2020"
             )
-            addShopItem(item)
+            addRequestItem(requestItem)
         }
     }
 
-    private fun addShopItem(requestItem: RequestItem) {
+    private fun addRequestItem(requestItem: RequestItem) {
         if(requestItem.id == RequestItem.DEFAULT_ID){
             requestItem.id = autoIncrementId++
         }
-        requestList.
+        requestList.add(requestItem)
+        updateLD()
     }
+
+    private fun deleteRequestItem(requestItem: RequestItem){
+        requestList.remove(requestItem)
+        updateLD()
+    }
+
+    private fun editRequestItem(requestItem: RequestItem){
+        val old_item = getRequestItem(requestItem.id)
+        requestList.remove(old_item)
+        addRequestItem(requestItem)
+    }
+
+    private fun getRequestItem(requestItemId: Int): RequestItem{
+        return requestList.find {
+            it.id == requestItemId
+        } ?: throw RuntimeException("Element with id $requestItemId not found")
+    }
+
+    private fun getRequestList(): LiveData<List<RequestItem>>{
+        return requestListLD
+    }
+
+    private fun updateLD(){
+        requestListLD.value = requestList
+    }
+
+
 }
--- app/src/main/java/ru/myrequest/presentation/MainActivity.kt
+++ app/src/main/java/ru/myrequest/presentation/MainActivity.kt
@@ -2,11 +2,15 @@ package ru.myrequest.presentation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import ru.myrequest.R
+import ru.myrequest.data.RequestListRepImplementation
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val item = RequestListRepImplementation.getRequestItem(11)
+        Log.d("MyLog", item.toString())
     }
 }
